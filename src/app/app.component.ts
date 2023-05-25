aa14989fc356427b5f84b5ac3a12f2c8bb3debbf
Anh Lai Nguyen Tuan <anh.lnt@umc.edu.vn>
2023-05-25 09:22:28 +0700

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'deploy-github-angular';
+	  constructor(
+			private _activeRoute: ActivatedRoute,
+		) {
+			this._activeRoute.params
+				.pipe(tap(param => {
+					const { id } = param;
+					window.open(`https://view.officeapps.live.com/op/embed.aspx?src=http://uofficeapi.umc.edu.vn/v1/form/415915138191444/static/1684830510288`, '_blank');
+				}),
+					takeUntil(this._destroyed))
+				.subscribe()
+
+		}
+}
